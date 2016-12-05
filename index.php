569376f69b544e8b35781a5f39d2f3e850a52e7a
matiasgarafoni <matias.garafoni@gmail.com>
2016-12-05 06:31:47 -0300

    added: <meta name="viewport" content="width=device-width, initial-scale=1.0">  to index.php for mobile adaptability.

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>RufoPHP - PHP Framework</title>
         <!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
